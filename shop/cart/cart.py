c12c9a450996155e4e230a729118d965f55a8409
anza-afk <zapokami@gmail.com>
2023-04-28 10:54:42 +0300

    fixes

--- shop/cart/cart.py
+++ shop/cart/cart.py
@@ -14,8 +14,10 @@ class Cart():
     def add(self, product, quantity=1, update_quantity=False):
         product_id = str(product.id)
         if product_id not in self.cart:
-            self.cart[product_id] = {'quantity': 0,
-                                     'price': str(product.price)}
+            self.cart[product_id] = {
+                'quantity': 0,
+                'price': str(product.price)
+            }
         if update_quantity:
             self.cart[product_id]['quantity'] = quantity
         else:
@@ -50,7 +52,7 @@ class Cart():
     def get_total_price(self):
         return sum(Decimal(
           item['price'])*item['quantity'] for item in self.cart.values()
-      )
+        )
 
     def clear(self):
         del self.session['cart']
--- shop/products/views.py
+++ shop/products/views.py
@@ -1,8 +1,8 @@
 from django.views.generic import TemplateView
-from django.http import HttpResponse
 from django.views.generic import ListView
 from products.models import Product
 from cart.forms import CartAddProductForm
+
 
 class IndexView(TemplateView):
     template_name = "index.html"
@@ -12,8 +12,10 @@ class ProductListView(ListView):
     model = Product
     paginate_by = 5
     
-    def get_context_data(self,**kwargs):
+    def get_context_data(self, **kwargs):
         context = super(
-            ProductListView,self).get_context_data(**kwargs)
-        context['cart_product_form']=CartAddProductForm(self.request.GET or None)
+            ProductListView, self).get_context_data(**kwargs)
+        context['cart_product_form'] = CartAddProductForm(
+            self.request.GET or None
+            )
         return context
